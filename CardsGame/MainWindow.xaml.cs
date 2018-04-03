83a2a43488082d52f5f76e3cae8822e6af2c83b2
ingTorok <toroklehel89@gmail.com>
2018-04-03 12:01:54 +0200

    #2 Creating the GameSpace Class Defining first variables

--- CardsGame/MainWindow.xaml.cs
+++ CardsGame/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CardsGame.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,13 @@ namespace CardsGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        GameSpace GameSpace;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            GameSpace = new GameSpace(this);
         }
     }
 }
---
+++ CardsGame/Models/GameSpace.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardsGame.Models
+{
+    /// <summary>
+    /// This class is responsible for the game operation
+    /// </summary>
+    class GameSpace
+    {  
+        /// <summary>
+        /// The MainWindow where the game is running
+        /// </summary>
+        private MainWindow MainWindow;
+        
+        /// <summary>
+        /// Remaining time from the actually game
+        /// </summary>
+        private TimeSpan RemainedTime;
+
+        /// <summary>
+        /// Reached points
+        /// </summary>
+        private int TotalPoints = 0;
+
+        /// <summary>
+        /// Show the Reaction Time
+        /// </summary>
+        private int ReactionTime = 0;
+
+        /// <summary>
+        /// Constructor, on creation get the MainWindow
+        /// </summary>
+        /// <param name="mainWindow">Window, which is showed on game progress</param>
+        public GameSpace(MainWindow mainWindow)
+        {
+            this.MainWindow = mainWindow;
+            SetNewGameCounters();
+
+        }
+   
+        /// <summary>
+        /// CardDeck, here are stored the cards
+        /// </summary>
+        public CardDeck CardDeck { get; private set; }
+
+        /// <summary>
+        /// Set the game counters for Start.
+        /// </summary>
+        private void SetNewGameCounters()
+        {
+            RemainedTime = TimeSpan.FromSeconds(45);
+            CardDeck = new CardDeck();
+            ShowGameCounters();
+        }
+
+        /// <summary>
+        /// show and refresh the gama counters
+        /// </summary>
+        private void ShowGameCounters()
+        {
+            MainWindow.LabelCountDown.Content = $"Time remain: {RemainedTime.ToString("mm//:ss")}";
+            MainWindow.LabelPoints.Content = $"Points {TotalPoints}";
+            MainWindow.LabelReaction.Content = $"Reaction Time {ReactionTime}";
+        }
+    }
+}
